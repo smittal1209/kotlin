0c3cd91b0896844cc27f0eba1389fa87191fd66f
shashankmittal <shashank.mittal@telekom-digital.com>
2019-10-15 19:38:04 +0530

    InitalCrudSetup

--- src/main/kotlin/com/dt/kotlin/KotlinStarterApplication.kt
+++ src/main/kotlin/com/dt/kotlin/KotlinStarterApplication.kt
@@ -1,9 +1,10 @@
 package com.dt.kotlin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 class KotlinStarterApplication
 
 fun main(args: Array<String>) {
